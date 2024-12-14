7d2058cc8c1e1763fb3b03dc01be19157d5d53ef
ClemenceKyende <mwendeclemence@gmail.com>
2024-12-14 13:25:06 +0300

    Initial commit for SwiftNote

--- swiftnote/wsgi.py
+++ swiftnote/wsgi.py
@@ -1,13 +1,10 @@
-"""
-WSGI config for swiftnote project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add your project directory to the sys.path
+path = '/home/ClemenceKyende/swiftnote'  # Update with the correct path
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
